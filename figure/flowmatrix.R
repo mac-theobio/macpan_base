259c2a399d654f3f3208eb4da98c5290225f0208
Ben Bolker <bbolker@gmail.com>
2022-07-25 16:04:43 -0400

    continuing to write/tweak

--- figure/flowmatrix.R
+++ figure/flowmatrix.R
@@ -5,6 +5,7 @@ pp <- read_params("ICU1.csv")
 pp <- fix_pars(pp)
 
 pdf("figure/flowchart.pdf",height=8,width=8)
+
 par(mar=rep(0,4))
 vis_model(pp,method="diagram",
           do_symbols=FALSE,
@@ -12,8 +13,11 @@ vis_model(pp,method="diagram",
           box.size=0.05,
           curve = 0,
           arr.type = "triangle",
-          shadow.size = 0
-)
+          shadow.size = 0,
+          arr.length = 0.2,
+          arr.width = 0.2
+          )
+
 dev.off()
 knitr::plot_crop("figure/flowchart.pdf")
 
