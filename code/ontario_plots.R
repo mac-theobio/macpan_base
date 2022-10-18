ab9f78f5fe326c1a420c9286be4aeeee9231cc8e
wzmli <wzmichael.li@gmail.com>
2022-10-18 03:25:12 -0400

    forgot to push plots

--- code/ontario_plots.R
+++ code/ontario_plots.R
@@ -1,7 +1,30 @@
-library(tidyverse)
+library(tidyverse); theme_set(theme_bw(base_size = 10))
 library(McMasterPandemic)
 
 base <- readRDS("cachestuff/ont_calib_comb_mobbreaks.rds")
-plot(base$fit,data=base$data)
+plot(base$fit,data=filter(base$data,date<=as.Date("2020-08-30")))
+ggsave("ontario_base.png")
 
+testify <- readRDS("cachestuff/ont_calib_testify.rds")
 
+dd <- predict(testify$fit
+			  , ensemble=FALSE
+			  , keep_vars=c("postest","death","H")
+)
+
+ddcombo <- (testify$data
+	%>% mutate(var = ifelse(var == "report","postest",var))
+	%>% transmute(date, var,data=value)
+	# %>% gather(key = "var", value="data",-date)
+	%>% left_join(dd,.)
+)
+
+gg <- (ggplot(ddcombo, aes(x=date, color=var))
+	   + geom_line(aes(y=value))
+	   + geom_point(aes(y=data),alpha=0.3)
+	   + scale_y_log10(limits=c(1,NA))
+	   + theme_bw()
+)
+
+print(gg)
+ggsave("ontario_testify.png")
