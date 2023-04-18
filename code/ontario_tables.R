eab0680db1ad4f9c08978e55deaca5372e3cdf42
Ben Bolker <bbolker@gmail.com>
2023-04-18 12:04:37 -0400

    clean up tables, add litparmtab

--- code/ontario_tables.R
+++ code/ontario_tables.R
@@ -6,86 +6,82 @@ library(shellpipes)
 base <- readRDS("code/cachestuff/ont_calib_comb_mobbreaks.rds")
 testify <- readRDS("code/cachestuff/ont_calib_testify.rds")
 
-get_params_table <- function(x){
-	coef_table <- data.frame()
-	cc <- coef(x,"fitted")
-	for(i in names(cc)){
-		tempdat <- data.frame(type = i
-			, pars = names(cc[[i]])
-			, value = cc[[i]]
-		)
-		coef_table <- bind_rows(tempdat,coef_table)
-		rownames(coef_table) <- NULL
-	}
-	return(coef_table)
+beautify <- function(x) {
+    x[,"symbol"] <- texify(as.character(x[,"symbol"]), force=TRUE)
+    return(x)
 }
 
-base_table <- get_params_table(base$fit)
+get_params_table <- function(x){
+    coef_table <- data.frame()
+    cc <- coef(x,"fitted")
+    for(i in names(cc)){
+        tempdat <- data.frame(type = i
+                            , pars = names(cc[[i]])
+                            , value = cc[[i]]
+                              )
+        coef_table <- bind_rows(tempdat,coef_table)
+        rownames(coef_table) <- NULL
+    }
+    return(coef_table)
+}
+
+breakpoints <- c("Apr 01", "Apr 07")
+par_names <- c(
+    sprintf("Relative change in transmission after %s", breakpoints)
+  , "Mobility power"
+  , sprintf("Change in mobility power after %s", breakpoints)
+  , sprintf("Negative binomial dispersion parameter (%s)",
+            c("case reports", "deaths"))
+  , "Initial number exposed"
+  , "Transmission rate"
+  , "Non-hospitalized mortality probability"
+  , "Exponent of phenomenological response  to susceptible depletion"
+)
+
 testify_table <- get_params_table(testify$fit)
-base_table
-testify_table
 
-testify_pardf <- data.frame(pars = testify_table$pars
-	, names = c("Mobility Intercept Apr 01"
-		, "Mobility Intercept Aug 07"
-		, "Mobility Slope"
-		, "Mobility Slope Apr 01"
-		, "Mobility Slope Aug 07"
-		, "Observation Error"
-		, "E0"
-		, "Beta0"
-		, "Zeta"
-	)
+mk_table <- function(fit, par_names) {
+    (fit
+    |> get_params_table()
+    |> transmute(names = par_names, value)
+    |> mutate(across(value, ~pmin(., 1000))) ## truncate NB dispersions - don't mess up formatting for everyone else
+    |> mutate(fval = format(round(value, 3)))
+    |> mutate(across(fval, ~ ifelse(value == 1000, "$\\gg 1000$", .)))
+    |> select(-value)
+    |> setNames(c("Parameter", "Estimate"))
+    )
+}
+
+write_table <- function(table, fn) {
+    invisible(Hmisc::latex(table
+                         , col.just = c("l", "r")
+                         , collabel.just=c("l","r")
+                         , rowname = NULL
+                         , file = fn
+                         , table.env = FALSE
+                           ))
+}
+
+base_pardf <- mk_table(base$fit, par_names)
+write_table(base_pardf, "base_table.tex")
+
+testify_pardf <- mk_table(testify$fit,
+                          grep("(death|mortality)", par_names, invert = TRUE, value = TRUE))
+
+
+write_table(testify_pardf, "testify_table.tex")
+
+pp <- update(read_params("PHAC_testify.csv"), testing_intensity=0) # reads from pkg if not found locally
+attr(pp,"description") <- NULL
+descr <- suppressWarnings(describe_params(pp))
+
+lit_param_table <- (
+    descr
+    |> dplyr::filter(!(symbol %in% c("beta0","N","E0","Multiple","mu","iso_m","iso_s")))
+    |> dplyr::select(meaning, value)
+    |> setNames(c("Parameter", "Value"))
 )
 
-base_pardf <- data.frame(pars = base_table$pars
-	, names = c("Mobility Intercept Apr 01"
-			, "Mobility Intercept Aug 07"
-			, "Mobility Slope"
-			, "Mobility Slope Apr 01"
-			, "Mobility Slope Aug 07"
-			, "Observation Error Report"
-			, "Observation Error Death"
-			, "E0"
-			, "Beta0"
-			, "Non-hospitalized Mortality"
-			, "Zeta"
-	)
-)
-
-base_table_rename <- (left_join(base_table,base_pardf)
-	%>% transmute(NULL
-			, Parameter = names
-			, Estimate = ifelse(value > 1000,1000,value)
-			, Estimate = as.character(format(round(Estimate,3)),nsmall=3)
-			, Estimate = ifelse(as.numeric(Estimate) == 1e3, "\\gg 1000",Estimate)
-	)
-)
-
-print(base_table_rename)
-
-testify_table_rename <- (left_join(testify_table,testify_pardf)
-	%>% rowwise()
-	%>% transmute(NULL
-		, Parameter = names
-		, Estimate = as.character(format(round(value,3)),nsmall=3)
-		, Estimate = ifelse(as.numeric(Estimate) > 1e3, "$\\gg 1000$",Estimate)
-	)
-)
-
-testify_table_rename
-
-cat(kable(base_table_rename,format="latex",align = c("l","r"))
-          ## could use decimal alignment but have other stuff to sort out ...
-          ## "d{3.1}"))
-  , file = "base_table.tex"		
-)
-    
-cat(kable(testify_table_rename,format="latex",align = "c")
-	, file = "testify_table.tex"		
-)
-
-
-saveVars(testify_table_rename)
+write_table(lit_param_table, "litparmtab.tex")
 
 
