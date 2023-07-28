be66b777fe0a649716a065972b4442f7a185fbc1
Ben Bolker <bbolker@gmail.com>
2023-07-28 19:59:50 -0400

    update Rt/Reff plot

--- code/ontario_base_forecast_plot.R
+++ code/ontario_base_forecast_plot.R
@@ -5,10 +5,10 @@ library(cowplot)
 library(zoo)
 library(gtable)
 
+rpcall("code/ontario_base_forecast_plot.Rout .pipestar slow/ontario_base_forecast.rda")
+
 loadEnvironments()
 
-## FIXME: use this
-get_R0(coef(mod$fit), method = "analytical")
 
 reports_death <- (forecast
 	%>% filter(var %in% c("report","death"))
@@ -22,14 +22,18 @@ reports_death <- (forecast
 
 print(reports_death,n=Inf)
 
+R0val <- get_R0(coef(mod$fit), method = "analytical")
+
 rt_ensemble <- (forecast
 	%>% filter(var %in% c("S","foi","I"))
 	%>% pivot_longer(names_to="seed",values_to="est",-c("date","var"))
 	%>% spread(var,est)
 	%>% group_by(seed)
 	%>% mutate(S0 = first(S)
-		, beta = foi/I
-		, rt = beta*S0
+                 , beta = foi/I
+                 , beta0 = first(beta)
+                 , rt = R0val*beta/beta0,
+                 , reff = rt*S/S0
 	)
 	%>% ungroup()
 )
@@ -37,12 +41,13 @@ rt_ensemble <- (forecast
 ## rt_ensemble |> filter(date == min(date)) |> pull(rt) |> hist()
 
 rt <- (rt_ensemble
-    %>% select(date,seed,rt)
-    %>% group_by(date)
-    %>% summarise(value = quantile(rt,prob=0.5,na.rm=TRUE)
-                , lwr = quantile(rt, prob=0.025,na.rm=TRUE)
-                , upr = quantile(rt, prob=0.975,na.rm=TRUE)
-                  )
+    %>% select(date,seed,rt, reff)
+    %>% pivot_longer(c(rt, reff), names_to = "type", values_to = "value")
+    %>% group_by(date, type)
+    %>% summarise(med = quantile(value,prob=0.5,na.rm=TRUE)
+                , lwr = quantile(value, prob=0.025,na.rm=TRUE)
+                , upr = quantile(value, prob=0.975,na.rm=TRUE)
+                , .groups = "drop")
 )
 
 print(rt)
@@ -76,11 +81,12 @@ gg_death <- (ggplot(filter(reports_death,var=="death"),aes(x=date,y=value))
 ggcombo <- plot_grid(gg_report,gg_death,nrow=2)
 teeGG(ggcombo, desc="rptDeath")
 
-(ggplot(rt,aes(x=date,y=value))
-    + geom_line()
-    + geom_ribbon(aes(ymin=lwr,ymax=upr),alpha=0.1)
+(ggplot(rt,aes(x=date,y=med))
+    + geom_line(aes(linetype = type))
+    + geom_ribbon(aes(ymin=lwr,ymax=upr, group = type),alpha=0.1)
     + theme_bw()		
-    + ylab("Rt")
+    + ylab("Reproductive number")
     + geom_hline(aes(yintercept=1))
     + scale_x_date(date_labels = "%b-%Y")
+    + scale_linetype_manual(values = 1:2, labels = c("Reff", "Rt"))
 ) %>% teeGG(desc="rt")
