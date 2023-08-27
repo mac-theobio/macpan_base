ae01634bf29dc26addec53d5bec46000d86268af
Ben Bolker <bbolker@gmail.com>
2023-08-27 12:41:40 -0400

    working on refitting mle, hope to address non-pos-def hessian

--- code/ontario_base_calibration_check.R
+++ code/ontario_base_calibration_check.R
@@ -3,14 +3,27 @@ library(dplyr)
 library(bbmle)
 library(shellpipes)
 
+rpcall("code/ontario_base_calibration_check.R slow/ontario_calibrate_comb.rds")
+
 mod <- rdsRead()
 
-print(mod$fit$mle2@details$convergence)
+mle2_obj <- mod$fit$mle2
+
+## optim thinks convergence is OK:
+print(mle2_obj@details$convergence)
 
 ##print(summary(mod$fit$mle2))
 
-print(diag(mod$fit$mle2@vcov))
+print(diag(vcov(mle2_obj)))
 
-pp <- profile(mod$fit$mle2, std.err=.01, trace=TRUE, devtol=Inf)
+optargs <- c(list(par = coef(mle2_obj),
+                  fn = mle2_obj@minuslogl,
+                  data = mod$data),
+             mod$fit$forecast_args)
+newopt <- do.call("optim", optargs)
+                  
+## pp <- profile(mod$fit$mle2, std.err=.01, trace=TRUE, devtol=Inf)
 
-rdsSave(pp)
+##rdsSave(pp)
+rdsSave(newopt)
+
